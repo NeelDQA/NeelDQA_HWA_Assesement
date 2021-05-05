53468813b18021b512f392ddae4ac9c745cb7aca
NeelDQA <NDudhia@qa.com>
2021-05-05 22:31:59 +0100

    ND: editing repo file

--- HWA-Asseement-NeelDudhia/src/main/java/com/example/musicmanagement/repo/SongRepo.java
+++ HWA-Asseement-NeelDudhia/src/main/java/com/example/musicmanagement/repo/SongRepo.java
@@ -4,10 +4,12 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.musicmanagement.domain.Song;
 
-public interface  SongRepo extends JpaRepository<Song, Long> {
-	
+public interface SongRepo extends JpaRepository<Song, Long> {
+
 	Song findBysongName(String songName);
+
 	Song findByartistName(String artistName);
+
 	Song findBygenre(String genre);
 
 }
